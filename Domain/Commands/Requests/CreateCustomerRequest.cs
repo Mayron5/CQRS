123d4b045814b495a177f3b053fe69ef195c088c
Mayron Alves <mayronalves@alu.ufc.br>
2022-12-17 10:29:28 -0300

    Adicionando README.md e a classe do Requests

--- Domain/Commands/Requests/CreateCustomerRequest.cs
+++ Domain/Commands/Requests/CreateCustomerRequest.cs
@@ -3,4 +3,12 @@ namespace CQRS.Domain.Commands.Requests;
 public class CreateCustomerRequest
 {
 
+    public CreateCustomerRequest(string Name, string Email)
+    {
+        this.Name = Name;
+        this.Email = Email;
+    }
+
+    public string Name { get; set; }
+    public string Email { get; set; }
 }
